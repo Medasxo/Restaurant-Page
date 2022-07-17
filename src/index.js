e2a77c5a22914fafa36621878618bf534216aecb
Medas <medas02345@gmail.com>
2022-07-17 21:30:35 +0300

    Add a logic of switching corresponding pages after clicking menu, contacts. (Now only with menu)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import './style.css';
+
 import {loadMain} from "../src/loadMain";
+import {loadMenu} from "../src/loadMenu";
 
 
 loadMain();
+const menuLink = document.querySelector(".menu");
+menuLink.addEventListener('click', () => {
+    const contentContainer = document.getElementById("content");
+    let child = contentContainer.lastElementChild;
+    while(child){
+        contentContainer.removeChild(child);
+        child = contentContainer.lastElementChild;
+    }
+    loadMenu();
+});
+
 
 
 
---
+++ src/loadMenu.js
@@ -0,0 +1,6 @@
+import './styleMenu.css';
+export function loadMenu(){
+    const contentContainer = document.getElementById("content");
+    const siuContainer = document.createElement("div");
+    contentContainer.appendChild(siuContainer);
+}
